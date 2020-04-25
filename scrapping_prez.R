7d467e807ec342ca5b8074956909a2643f3db4fb
SquintRook <mateuszdadej@gmail.com>
2020-04-25 20:53:24 +0200

    added ridge plot

--- scrapping_prez.R
+++ scrapping_prez.R
@@ -3,37 +3,41 @@ library(rvest)
 library(tidyverse)
 library(lubridate)
 
+############### pobieranie sondaży wszystkich kandydatów ###############
+
 url <- xml2::read_html("http://ewybory.eu/wybory-prezydenckie-2020/sondaze-prezydenckie/")
-df <- rvest::html_table(url, fill = TRUE)[[1]]%>%
+sond_prez <- rvest::html_table(url, fill = TRUE)[[1]]%>%
   .[-(1:2),-c(10:ncol(.))]
   
-colnames(df) <- c("agencja", "pub", "data_bad", "RB", "MKB", "WKK", "SzH", "AD", "KB")
+colnames(sond_prez) <- c("agencja", "pub", "data_bad", "RB", "MKB", "WKK", "SzH", "AD", "KB")
 
-df <- mutate(df, agencja = as.character(agencja),
-             pub_d = str_split_fixed(df$pub, "\\.", n = 2)[,1],
-             pub_mies = str_split_fixed(df$pub, "\\.", n = 2)[,2],
+sond_prez <- mutate(sond_prez, agencja = as.character(agencja),
+             pub_d = str_split_fixed(sond_prez$pub, "\\.", n = 2)[,1],
+             pub_mies = str_split_fixed(sond_prez$pub, "\\.", n = 2)[,2],
              pub_data = NA,
              bad_ = str_split_fixed(data_bad, "-", n = 2)[,2],
              bad_data = NA)
 
-df[,4:9] <- apply(df[,4:9], 2, as.numeric)
+sond_prez[,4:9] <- apply(sond_prez[,4:9], 2, as.numeric)
 
-df$bad_[df$bad_ == ""] <- df$data_bad[df$bad_ == ""]
-df$bad_[df$bad_ == "?"] <- NA
+sond_prez$bad_[sond_prez$bad_ == ""] <- sond_prez$data_bad[sond_prez$bad_ == ""]
+sond_prez$bad_[sond_prez$bad_ == "?"] <- NA
 
-df <- mutate(df, bad_d = str_split_fixed(bad_, "\\.", n = 2)[,1],
+sond_prez <- mutate(sond_prez, bad_d = str_split_fixed(bad_, "\\.", n = 2)[,1],
              bad_mie = str_split_fixed(bad_, "\\.", n = 2)[,2])
 
-df$pub_data <- as.Date(with(df, paste("2020", pub_mies, pub_d ,sep="-")), "%Y-%m-%d")
-df$bad_data <- as.Date(with(df, paste("2020", bad_mie, bad_d ,sep="-")), "%Y-%m-%d")
+sond_prez$pub_data <- as.Date(with(sond_prez, paste("2020", pub_mies, pub_d ,sep="-")), "%Y-%m-%d")
+sond_prez$bad_data <- as.Date(with(sond_prez, paste("2020", bad_mie, bad_d ,sep="-")), "%Y-%m-%d")
 
-df <- select(df,-c(data_bad, pub, pub_d, pub_mies, bad_, bad_d, bad_mie ))%>%
+sond_prez <- select(sond_prez,-c(data_bad, pub, pub_d, pub_mies, bad_, bad_d, bad_mie ))%>%
   arrange(pub_data)
 
-df$bad_data[is.na(df$bad_data)] <- df$pub_data[is.na(df$bad_data)]-4
+sond_prez$bad_data[is.na(sond_prez$bad_data)] <- sond_prez$pub_data[is.na(sond_prez$bad_data)]-4
 
-df$agencja[df$agencja == "Kantar/CAPI"] <- "Kantar"
-df$agencja[df$agencja == "Kantar/CATI"] <- "Kantar"
-df$agencja[df$agencja == "Kantar/CATI (10.05)"] <- "Kantar"
-df$agencja[df$agencja == "IBRiS (10.05)"] <- "IBRiS"
+sond_prez$agencja[sond_prez$agencja == "Kantar/CAPI"] <- "Kantar"
+sond_prez$agencja[sond_prez$agencja == "Kantar/CATI"] <- "Kantar"
+sond_prez$agencja[sond_prez$agencja == "Kantar/CATI (10.05)"] <- "Kantar"
+sond_prez$agencja[sond_prez$agencja == "IBRiS (10.05)"] <- "IBRiS"
 rm("url")
+
+
--- wiz_prez.R
+++ wiz_prez.R
@@ -1,35 +1,78 @@
 library(ggplot2)
 
-source("scrapping_prezydenckie.R")
+source("scrapping_prez.R")
+# alternatively:
+# https://raw.githubusercontent.com/SquintRook/wybory2019-20/master/scrapping_prez.R 
 
 ########## Wizualizacja szeregów czasowych #######
 
 alph <- 0.4      # przejrzystość punktów
 conf <- FALSE    # czy chcemy przedział ufności
-spn <- 0.4       # jak bardzo ma wygładzać geom_smooth
+spn <- 0.8       # jak bardzo ma wygładzać geom_smooth
+point_size <- 1.4
 
+sond_prez$agencja[sond_prez$agencja %in% c("CBOS", "Maison & Partners", "PPG", "United Survey", "IBSP")] <- "inne"
 
-ggplot(data = df)+
+ggplot(data = sond_prez)+
   scale_x_date(breaks = "1 weeks",
                date_labels = "%d/%m",
-               limits = c(min(df$bad_data), as.Date("2020-05-10")))+
-  geom_point(aes(x = bad_data, y = RB/100, color = "R. Biedroń"), alpha = alph)+
+               limits = c(min(sond_prez$bad_data), as.Date("2020-05-10")))+
+  geom_point(aes(x = bad_data, y = RB/100, color = "R. Biedroń", shape = agencja), size = point_size, alpha = alph)+
   geom_smooth(aes(x = bad_data, y = RB/100, color = "R. Biedroń"), se = conf, span = spn)+
-  geom_point(aes(x = bad_data, y = WKK/100, color = "W. Kosiniak-Kamysz"), alpha = alph)+
+  geom_point(aes(x = bad_data, y = WKK/100, color = "W. Kosiniak-Kamysz", shape = agencja), size = point_size, alpha = alph)+
   geom_smooth(aes(x = bad_data, y = WKK/100, color = "W. Kosiniak-Kamysz"), se = conf, span = spn)+
-  geom_point(aes(x = bad_data, y = SzH/100, color = "S. Hołownia"), alpha = alph)+
+  geom_point(aes(x = bad_data, y = SzH/100, color = "S. Hołownia", shape = agencja), size = point_size, alpha = alph)+
   geom_smooth(aes(x = bad_data, y = SzH/100, color = "S. Hołownia"), se = conf, span = spn)+
-  geom_point(aes(x = bad_data, y = AD/100, color = "A. Duda"), alpha = alph)+
+  geom_point(aes(x = bad_data, y = AD/100, color = "A. Duda", shape = agencja), size = point_size, alpha = alph)+
   geom_smooth(aes(x = bad_data, y = AD/100, color = "A. Duda"), se = conf, span = spn)+
-  geom_point(aes(x = bad_data, y = KB/100, color = "K. Bosak"), alpha = alph)+
+  geom_point(aes(x = bad_data, y = KB/100, color = "K. Bosak", shape = agencja), size = point_size, alpha = alph)+
   geom_smooth(aes(x = bad_data, y = KB/100, color = "K. Bosak"), se = conf, span = spn)+
-  geom_point(aes(x = bad_data, y = MKB/100, color = "M. Kidawa-Błońska"), alpha = alph)+
+  geom_point(aes(x = bad_data, y = MKB/100, color = "M. Kidawa-Błońska", shape = agencja), size = point_size, alpha = alph)+
   geom_smooth(aes(x = bad_data, y = MKB/100, color = "M. Kidawa-Błońska"), se = conf, span = spn)+
-  labs(x = "", y = "poparcie", caption = "źródło: ewybory.eu, twitter: @mateusz_dadej",
+  labs(x = "", y = "", caption = "twitter: @mateusz_dadej, źródło: ewybory.eu",
        title = "Wyniki sondaży dla głównych kandydatów w wyborach prezydenckich")+
   scale_y_continuous(labels = scales::percent_format(accuracy = 1), breaks = seq(0,0.6,0.1))+
   theme_minimal()+
   theme(legend.position = "bottom", legend.title = element_blank())+
   scale_color_manual(values = c("coral2", "tan3", "steelblue3", "darkorchid3", "gold1", "springgreen3"))
 
+########## to samo ale bez dudy #############
 
+ggplot(data = select(sond_prez, - AD))+
+  scale_x_date(breaks = "1 weeks",
+               date_labels = "%d/%m",
+               limits = c(min(sond_prez$bad_data), as.Date("2020-05-10")))+
+  geom_point(aes(x = bad_data, y = RB/100, color = "R. Biedroń", shape = agencja), size = point_size, alpha = alph)+
+  geom_smooth(aes(x = bad_data, y = RB/100, color = "R. Biedroń"), se = conf, span = spn)+
+  geom_point(aes(x = bad_data, y = WKK/100, color = "W. Kosiniak-Kamysz", shape = agencja), size = point_size, alpha = alph)+
+  geom_smooth(aes(x = bad_data, y = WKK/100, color = "W. Kosiniak-Kamysz"), se = conf, span = spn)+
+  geom_point(aes(x = bad_data, y = SzH/100, color = "S. Hołownia", shape = agencja), size = point_size, alpha = alph)+
+  geom_smooth(aes(x = bad_data, y = SzH/100, color = "S. Hołownia"), se = conf, span = spn)+
+  geom_point(aes(x = bad_data, y = KB/100, color = "K. Bosak", shape = agencja), size = point_size, alpha = alph)+
+  geom_smooth(aes(x = bad_data, y = KB/100, color = "K. Bosak"), se = conf, span = spn)+
+  geom_point(aes(x = bad_data, y = MKB/100, color = "M. Kidawa-Błońska", shape = agencja), size = point_size, alpha = alph)+
+  geom_smooth(aes(x = bad_data, y = MKB/100, color = "M. Kidawa-Błońska"), se = conf, span = spn)+
+  labs(x = "", y = "", caption = "twitter: @mateusz_dadej, źródło: ewybory.eu",
+       title = "Wyniki sondaży dla kandydatów w wyborach prezydenckich z pominięciem PAD")+
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1), breaks = seq(0,0.3,0.05))+
+  theme_minimal()+
+  theme(legend.position = "right", legend.title = element_blank())+
+  scale_color_manual(values = c("tan3", "steelblue3", "darkorchid3", "gold1", "springgreen3"))
+
+######## ridge plot #############
+
+library(ggplot2movies)
+
+pivot_longer(sond_prez, cols = c("RB", "MKB", "WKK", "SzH", "AD", "KB"))%>%
+  mutate(value = value/100)%>%
+  ggplot(aes(x = value, y = name, group = name))+
+  geom_density_ridges_gradient(aes(fill = ..x..), scale = 1.5, rel_min_height = 0.00001, jittered_points = FALSE)+
+  scale_fill_continuous(type = "viridis", name = "", option = "C",labels = scales::percent)+
+  scale_x_continuous(labels = scales::percent_format(accuracy = 1L), breaks = seq(0,0.7, 0.1))+
+  scale_y_discrete(labels = c("A. Duda", "K. Bosak", "M. Kidawa-Błońska", "R. Biedroń", "S. Hołownia", "W. Kosianiak-Kamysz"))+
+  labs(title = "Rozkład poparcia dla kandydatów na prezydenta w wyborach 2020", y = "",
+       x = "poparcie", caption = "Twitter: @SquintRook, dane: ewybory.eu",
+       subtitle = "Wyniki sondaży wyborczych prowadzonych od listopada 2019 do konca kwietnia 2020")+
+  theme_minimal()
+  
+  
